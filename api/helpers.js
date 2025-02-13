a4b7824e0b4a9c588bb18bbd107ed79fa8a8f1d3
Oliver Foster <oliver.foster@kineo.com>
2025-02-13 18:02:58 +0000

    Simplified

--- api/helpers.js
+++ api/helpers.js
@@ -1,13 +1,17 @@
 import Task from '../lib/Task.js'
 
+function getContent() {
+  return Task.current.context.content;
+}
+
 export function getConfig () {
-  return Task.current.context.content.find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
+  return getContent().find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
 }
 
 export function getCourse() {
-  return Task.current.context.content.find(({ _type }) => _type === 'course');
+  return getContent().find(({ _type }) => _type === 'course');
 }
 
 export function getComponents(componentName) {
-  return Task.current.context.content.filter(({ _component }) => _component === componentName);
+  return getContent().filter(({ _component }) => _component === componentName);
 }
