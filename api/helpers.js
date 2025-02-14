b66a8e6b4a318deb7824b87698cf3962e4dfe3a7
Oliver Foster <oliver.foster@kineo.com>
2025-02-14 11:48:35 +0000

    New: Added getComponents with context (#35)

--- api/helpers.js
+++ api/helpers.js
@@ -1,7 +1,17 @@
-export function getConfig (content) {
-  return content.find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
+import Task from '../lib/Task.js'
+
+function getContent() {
+  return Task.current.context.content;
 }
 
-export function getCourse(content) {
-  return content.find(({ _type }) => _type === 'course');
+export function getConfig () {
+  return getContent().find(({ _type, __path__ }) => _type === 'config' || __path__.endsWith('config.json'))
+}
+
+export function getCourse() {
+  return getContent().find(({ _type }) => _type === 'course');
+}
+
+export function getComponents(componentName) {
+  return getContent().filter(({ _component }) => _component === componentName);
 }
